085390fefaf15b67fa8fb20c5434cbec331e9315
hirotk <hiro_tk@outlook.com>
2014-03-11 09:19:50 +0900

    Added SinglyLinkedList class.

---
+++ Algorithm/SinglyLinkedList.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Algorithm {
+    class SinglyLinkedListNode<T> {
+        public readonly T val;
+        public SinglyLinkedListNode<T> Next {get; set;}
+
+        public SinglyLinkedListNode(T val) { this.val = val; }
+    }
+
+    public class SinglyLinkedList<T> {
+        private readonly SinglyLinkedListNode<T> head;
+        private SinglyLinkedListNode<T> lastNode;
+
+        private int leng;
+        public int Leng {get{return leng;}}
+
+        public SinglyLinkedList() { 
+            head = new SinglyLinkedListNode<T>(default(T));
+            leng = 0;
+            lastNode = head;
+        }
+
+        private SinglyLinkedListNode<T> getPrevNode(int idx) {
+            if (idx < 0 || leng < idx) throw new ArgumentException("The idx is out of range.");
+            var iNode = head;
+            for (int i = 0; i < idx; ++i) { iNode = iNode.Next; }
+            return iNode;
+        }
+
+        public void Add(T val, int idx = 0){
+            try {
+                var iNode = getPrevNode(idx);
+                var node = new SinglyLinkedListNode<T>(val);
+
+                // Add node
+                node.Next = iNode.Next;
+                iNode.Next = node;
+
+                // Update LastNode
+                if (idx == leng) lastNode = node;
+                leng++;
+            } catch (Exception e) {
+                Console.WriteLine(e.Message + e.StackTrace);
+            }
+        }
+
+        public void AddLast(T val) {
+            var node = new SinglyLinkedListNode<T>(val);
+
+            // Add node
+            node.Next = lastNode.Next;
+            lastNode.Next = node;
+
+            // Update LastNode
+            lastNode = node;
+            leng++;
+        }
+
+        public T Remove(int idx = 0) {
+            try {
+                if (leng < 1) throw new Exception("The SinglyLinkedList is empty.");
+                var iNode = getPrevNode(idx);
+
+                // Remove node
+                var node = iNode.Next;
+                iNode.Next = node.Next;
+                
+                // Update LastNode
+                if (idx == leng - 1) lastNode = iNode;
+                leng--;
+                return node.val;
+            } catch (Exception e) {
+                Console.WriteLine(e.Message + e.StackTrace);
+                return default(T);
+            }
+        }
+
+        public T Peek(int idx = 0) {
+            try {
+                if (leng < 1) throw new Exception("The SinglyLinkedList is empty.");
+                var iNode = getPrevNode(idx + 1);
+                return iNode.val;
+            } catch (Exception e) {
+                Console.WriteLine(e.Message + e.StackTrace);
+                return default(T);
+            }
+        }
+
+        public override string ToString() {
+            var sb = new System.Text.StringBuilder("[ ");
+            var node = head;
+            for (int i = 0; i < this.Leng; ++i) {
+                node = node.Next;
+                sb.AppendFormat("{0} ", node.val);
+            }
+
+            sb.Append("]");
+            return sb.ToString();
+        }
+    }
+}
---
+++ AlgorithmTest/SinglyLinkedListTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Text;
+using Algorithm;
+
+namespace AlgorithmTest {
+    [TestClass]
+    public class SinglyLinkedListTest {
+        static SinglyLinkedList<int> target;
+
+        [ClassInitialize]
+        public static void InitializeTarget(TestContext testContext) { }
+
+        [ClassCleanup]
+        public static void CleanupTarget() { }
+
+        [TestInitialize()]
+        public void BeginTestMethod() {
+            target = new SinglyLinkedList<int>();
+            Assert.AreEqual(target.Leng, 0);
+            target.Add(2);
+            target.Add(3, 1);
+            target.AddLast(5);
+        }
+
+        [TestCleanup()]
+        public void EndTestMethod() { }
+
+        [TestMethod]
+        public void AddTest() {
+            Assert.AreEqual(target.Leng, 3);
+            target.Add(-2);
+            target.Add(4, 3);
+            target.Add(7, 5);
+            string expected = "[ -2 2 3 4 5 7 ]";
+            string actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(target.Leng, 6);
+        }
+
+        [TestMethod]
+        public void AddLastTest() {
+            target.AddLast(7);
+            target.AddLast(11);
+            string expected = "[ 2 3 5 7 11 ]";
+            string actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(target.Leng, 5);
+        }
+
+        [TestMethod]
+        public void RemoveTest() {
+            target.Remove();
+            target.Remove(1);
+            target.AddLast(7);
+            string expected = "[ 3 7 ]";
+            string actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(target.Leng, 2);
+            
+            target.Remove(1);
+            target.Remove();
+            expected = "[ ]";
+            actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PeekTest() {
+            int expected = 3;
+            int actual = target.Peek(1);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ToStringTest() {
+            target.Add(7, 3);
+            string expected = "[ 2 3 5 7 ]";
+            string actual = target.ToString();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
